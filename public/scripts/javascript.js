c64f06bde18e15e939466b78695a4771c70f1389
Gustavo Alexandre <gustavoti717@gmail.com>
2021-09-01 01:02:32 -0300

    feat: products and configs API consumes

--- public/scripts/javascript.js
+++ public/scripts/javascript.js
@@ -1,67 +1,25 @@
-//JS DO CADASTRO JQUERY
-const nome = document.getElementById("nome");
-const email = document.getElementById("email");
-const pass = document.getElementById("pass");
-const cpf = document.getElementById("cpf");
-const data = document.getElementById("data");
-const cep = document.getElementById("cep");
-const telefone = document.getElementById("telefone");
+function verificar(event) {
+  event.preventDefault();
+  function checkHasNotTypedField() {
+    const list = [...document.querySelectorAll(".form-input")];
 
-function verificar(){
-  if(nome.value==""){
-  window.alert("Você esqueceu de preencher o nome")
-  return false;
-}
+    console.log(list.filter((input) => !input.value));
 
-else if(email.value==""){
-  window.alert("Você esqueceu de preencher o email")
-  return false;
-}
-
-else if(pass.value==""){
-  window.alert("Você esqueceu de preencher a senha")
-  return false;
-}
-
-else if(cpf.value==""){
-  window.alert("Você esqueceu de preencher o cpf")
-  return false;
-}
-else if(cep.value==""){
-  window.alert("Você esqueceu de preencher o cep")
-  return false;
-}
-else if(data.value==""){
-  window.alert("Você esqueceu de colocar a data")
-  return false;
-}
-
-else if(telefone.value==""){
-  window.alert("Você esqueceu de preencher o telefone")
-  return false;
-}
-
-else{
-  window.alert("Cadastro concluído")
+    return list.filter((input) => !input.value).length > 0;
+  }
+  if (checkHasNotTypedField()) {
+    alert("Existem campos vazios pendentes");
+    return false;
+  }
   return true;
-}
 }
 
 //JS DO LOGIN
 
-
-
-
-
-
 //MASCARA JQUERY
-$('#cpf').mask("999.999.999-99");
-$('#telefone').mask('(99) 99999-9999');
+$("#cpf").mask("999.999.999-99");
+$("#telefone").mask("(99) 99999-9999");
 $("#cep").mask("99.999-999");
-
-
-
-
 
 // document.getElementById('olho').addEventListener('mousedown', function() {
 //   document.getElementById('pass').type = 'text';
@@ -76,24 +34,19 @@ $("#cep").mask("99.999-999");
 //   document.getElementById('pass').type = 'password';
 // });
 
-
-  
 //   //javascript da LATERALBAR//*
 //   function openNav() {
 //     document.getElementById("mySidenav").style.width = "250px";
 //   }
 
-
 //   function closeNav() {
 //     document.getElementById("mySidenav").style.width = "0";
 //   }
-
 
 //   function openNav() {
 //     document.getElementById("mySidenav").style.width = "250px";
 //     document.getElementById("main").style.marginLeft = "250px";
 //   }
-
 
 //   function closeNav() {
 //     document.getElementById("mySidenav").style.width = "0";
@@ -111,4 +64,3 @@ $("#cep").mask("99.999-999");
 //     document.getElementById("main").style.marginLeft = "0";
 //     document.body.style.backgroundColor = "white";
 //   }
-
--- src/server.js
+++ src/server.js
@@ -1,49 +1,135 @@
-const express = require('express')
-var app = express();
-app.set('view engine', 'ejs');
+const express = require("express");
+const fetch = require("node-fetch");
 
+var app = express();
+app.set("view engine", "ejs");
 
 app.set("views", __dirname + "/pages/");
 
+app.use(express.json());
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
 app.use(express.static("public"));
 
-
-app.get('/cadastro', function (req, res) {
+app.get("/cadastro", function (req, res) {
   res.render("cadastro");
-})
+});
 
-app.get('/configs', function (req, res) {
+app.post("/cadastro", async function (req, res) {
+  const { name, email, password, cpf, date, telefone } = req.body;
+
+  const user = { name, email, password, cpf, dataNascimento: date, telefone };
+
+  console.log(user);
+
+  const response = await fetch("http://localhost:3000/users", {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(user),
+  });
+  console.log(await response.json());
+  return res.redirect("/configs");
+});
+
+app.get("/configs", function (req, res) {
   res.render("configs");
-})
+});
 
-app.get('/login', function (req, res) {
+app.post("/configs", async function (req, res) {
+  const {
+    nomeloja,
+    editbio,
+    cep,
+    uf,
+    cidade,
+    logradouro,
+    bairro,
+    numero,
+    complemento,
+  } = req.body;
+
+  const configs = {
+    nomeloja,
+    editbio,
+    cep,
+    uf,
+    cidade,
+    logradouro,
+    bairro,
+    numero,
+    complemento,
+  };
+
+  console.log(configs);
+
+  const response = await fetch("http://localhost:3000/configs", {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(configs),
+  });
+  console.log(await response.json());
+  return res.redirect("/index");
+});
+
+app.get("/login", function (req, res) {
   res.render("login");
-})
+});
 
-app.get('/contato', function (req, res) {
+app.get("/contato", function (req, res) {
   res.render("contato");
-})
+});
 
-app.get('/index', function (req, res) {
+app.get("/index", function (req, res) {
   res.render("index");
-})
+});
 
-app.get('/orders', function (req, res) {
+app.get("/orders", function (req, res) {
   res.render("orders");
-})
+});
 
-app.get('/products', function (req, res) {
+app.get("/products", function (req, res) {
   res.render("products");
-})
+});
 
-app.get('/sobre', function (req, res) {
+app.post("/products", async function (req, res) {
+  const { nameproduto, imagem, description, disponibilidade, preco, unidade } =
+    req.body;
+
+  const products = {
+    nameproduto,
+    imagem,
+    description,
+    disponibilidade,
+    preco,
+    unidade,
+  };
+
+  console.log(products);
+
+  const response = await fetch("http://localhost:3000/products", {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(products),
+  });
+  console.log(await response.json());
+  return res.redirect("/index");
+});
+
+app.get("/sobre", function (req, res) {
   res.render("sobre");
-})
+});
 
-
-
-
-
-app.listen(3000, function () {
-  console.log('Server is running : porta 3000!');
+app.listen(3001, function () {
+  console.log("Server is running : porta 3001!");
 });
